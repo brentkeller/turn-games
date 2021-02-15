b2cedb66231495ce29e1a3f13b11db85496f71c0
Brent Keller <brentkeller@gmail.com>
2021-02-14 23:37:39 -0500

    Update flex styling for app skeleton
    Remove justify-center from root, no need to vertically center everything

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { MainHeader } from '../components/common/MainHeader';
 import { SubHeader } from '../components/common/SubHeader';
 
 export const Home: React.FC = () => {
   return (
     <>
       <SubHeader />
-      <div className="container w-full p-20 m-4 mx-auto my-16 text-center bg-white border-2 border-dashed border-blueGray-300 h-96 rounded-xl">
+      <div className="flex-1 container w-full p-20 m-4 mx-auto my-16 text-center bg-white border-2 border-dashed border-blueGray-300 h-96 rounded-xl">
         <p className="mt-20 italic tracking-tighter text-md text-blueGray-500 title-font">
           -- Content goes here --
         </p>
