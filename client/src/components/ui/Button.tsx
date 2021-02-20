80400e8a08e36c8573c54d8f146ce1a9a3dbca2b
Brent Keller <brentkeller@gmail.com>
2021-02-19 23:50:19 -0500

    Expand style support for Button

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -1,19 +1,28 @@
 import * as React from 'react';
+import classNames from 'classnames';
 
 interface ButtonProps {
   onClick(): void;
+  buttonClasses?: string;
   children?: React.ReactNode;
   disabled?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({ children, disabled, onClick }) => {
+export const Button: React.FC<ButtonProps> = ({ children, buttonClasses, disabled, onClick }) => {
+  const classes = classNames(
+    buttonClasses,
+    `
+    px-8 py-2 font-semibold text-white rounded-lg shadow-xl
+    transition duration-500 ease-in-out transform bg-gradient-to-r from-blue-700
+    hover:from-blue-600 to-blue-600 hover:to-blue-700
+    focus:ring focus:outline-none
+  `,
+    {
+      'opacity-50 cursor-not-allowed': disabled,
+    },
+  );
   return (
-    <button
-      onClick={onClick}
-      disabled={disabled}
-      type="button"
-      className="px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg shadow-xl bg-gradient-to-r from-blue-700 hover:from-blue-600 to-blue-600 hover:to-blue-700 focus:ring focus:outline-none"
-    >
+    <button onClick={onClick} disabled={disabled} type="button" className={classes}>
       {children}
     </button>
   );
