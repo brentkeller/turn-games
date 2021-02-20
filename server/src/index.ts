c9f0827f26250840509126c213e471d727c8041c
Brent Keller <brentkeller@gmail.com>
2021-02-19 22:54:36 -0500

    Export ApiError interface from server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,4 +2,5 @@ export * from './database/models/game';
 export * from './database/models/stocks/stock';
 export * from './database/models/stocks/stocks-game-player';
 export * from './database/models/user';
+export * from './interfaces/api-error';
 export * from './util/date-helper';
--- server/src/middleware/error-middleware.ts
+++ server/src/middleware/error-middleware.ts
@@ -1,17 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
+import { ApiError } from '../interfaces/api-error';
 import HttpException from '../exceptions/http-exception';
 
 function errorMiddleware(
   error: HttpException,
-  request: Request,
+  _request: Request,
   response: Response,
-  next: NextFunction,
+  _next: NextFunction,
 ) {
   const status = error.status || 500;
   const message = error.message || 'Something went wrong';
   const data = error.data;
   console.log('error', error);
-  response.status(status).json({ message, data });
+  response.status(status).json({ message, data } as ApiError);
 }
 
 export default errorMiddleware;
